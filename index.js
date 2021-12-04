8e649085089a925ed1c6d7ed73457c96c0d534c9
longngo11 <ngolong94ct@gmail.com>
2021-12-04 15:28:47 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const { buildSchema } = require('graphql');
+const { graphqlHTTP } = require('express-graphql');
+
+/* Đây là ví dụ schema đã được khởi tạo */
+
+let schema = buildSchema(`
+  type Query {
+    postTitle: String,
+    blogTitle: String
+  }
+`);
+
+// Root provides a resolver function for each API endpoint
+let root = {
+  postTitle: () => {
+    return 'Build a Simple GraphQL Server With Express and NodeJS';
+  },
+  blogTitle: () => {
+    return 'scotch.io';
+  }
+};
+
+const app = express();
+app.use('/', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true //Set to false if you don't want graphiql enabled
+}));
+
+app.listen(4000,()=>console.log('GraphQL API server running at localhost:'));
---
+++ index2.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const { buildSchema } = require('graphql');
+const { graphqlHTTP } = require('express-graphql');
+/* Đây là ví dụ schema đã được khởi tạo */
+var cors = require('cors')
+const corsOptions ={
+  origin:'http://localhost:3000', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
+}
+var schema = buildSchema(`
+  type Query {
+    slider(id: Int!): Slider
+    sliders(topic:String): [Slider]
+  }
+  type Slider {
+    id: Int,
+    title: String
+    url: String
+    para: String
+  }
+
+`);
+var sliderdata =[
+  {
+      id:1,
+      title: "Guerrilla Public Service Redux (2017)",
+      url: "https://thegioidohoacom.s3.ap-southeast-1.amazonaws.com/wp-content/uploads/2018/12/12023827/thi%E1%BA%BFt-k%E1%BA%BF-banner-sinh-nh%E1%BA%ADt-4.png",
+      para: "23325319"
+  },
+  {
+    id:2,
+    title: "Guerrilla Public Service Redux (2017)",
+    url: "https://thegioidohoacom.s3.ap-southeast-1.amazonaws.com/wp-content/uploads/2018/12/12023827/thi%E1%BA%BFt-k%E1%BA%BF-banner-sinh-nh%E1%BA%ADt-4.png",
+    para: "23325319"
+  }
+]
+var  getSlider = function(args){
+  var id = args.id;
+  return sliderdata.filter(slider =>{
+    return slider.id==id;
+  })[0];
+
+}
+var  getSliders = function(args){
+    if(args.title){
+      var title=args.title;
+      return sliderdata.filter(slider=>slider.title == title
+      );
+    }else{
+      return sliderdata;  
+    }
+}
+// Root provides a resolver function for each API endpoint
+var root = {
+  slider:getSlider,
+  sliders:getSliders
+};
+
+const app = express();
+app.use('/', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true //Set to false if you don't want graphiql enabled
+}));
+
+app.listen(4000,()=>console.log('GraphQL API server running at localhost:'));
+app.use(cors(corsOptions));
